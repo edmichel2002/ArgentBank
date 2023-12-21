04aecb3f27fbf79d9c0be7d37763daef2fe09206
edmichel2002 <eddymichel2002@gmail.com>
2023-12-21 09:30:28 +0100

    editbutton

--- src/Components/EditButton/EditButton.jsx
+++ src/Components/EditButton/EditButton.jsx
@@ -1,48 +1,85 @@
-//import React from "react";
-import { useState } from "react";
+import  { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { setUserName } from '../../Redux/Reduce/profilReducer';
 import "./EditButton.css";
 import { apiCallWithAuth } from "../../Api/Apiservice";
 
-
-function setNewUsername(e, dispatch) {
-    const userName = e.target[0].value;
-    e.preventDefault();
-    // Mise a jour du store
-    dispatch(setUserName(userName))
-    // Mise a jour de l'API
-    apiCallWithAuth("PUT", "user/profile", localStorage.getItem("token"), {userName: userName})
-}
-
-function EditButton (){
+function EditButton() {
     const dispatch = useDispatch();
     const profil = useSelector((state) => state.profil);
     const userName = profil.userName;
     const firstName = profil.firstName;
     const lastName = profil.lastName;
-    const [toogle, setToogle] = useState(false);
+    const [toggle, setToggle] = useState(false);
+
+    const setNewUsername = (e) => {
+        e.preventDefault();
+        const newUserName = e.target[0].value;
+
+        // Mise à jour du store
+        dispatch(setUserName(newUserName));
+
+        // Mise à jour de l'API
+        apiCallWithAuth("PUT", "user/profile", localStorage.getItem("token"), { userName: newUserName });
+
+        // Fermer le formulaire après avoir sauvegardé
+        setToggle(false);
+    };
+
+    const onCancel = () => {
+        setToggle(false);
+    };
+
     return (
         <div>
-            <button className="edit-button" onClick={
-                () => {
-                    console.log(userName);
-                    console.log("Edit button clicked!");
-                    setToogle(!toogle);
-                }
-            }>Edit Name</button>
-            {toogle && <div>
-                <form onSubmit={(e) => {setNewUsername(e, dispatch)}}>
-                    <label>User Name : </label>
-                    <input type="text" placeholder="User Name" defaultValue={userName} />
-                    <label>First Name : </label>
-                    <input type="text" disabled placeholder="First Name" defaultValue={firstName} />
-                    <label>Last Name : </label>
-                    <input type="text" disabled placeholder="Last Name" defaultValue={lastName} />
-                    <button type="submit">Save</button>
-                    <button>Cancel</button>
+            <button className="edit-button" onClick={() => setToggle(!toggle)}>
+                Edit Name
+            </button>
+            {toggle && (
+                <form onSubmit={(e) => setNewUsername(e)}>
+                    <div className='editProfil-main'>
+                        <h1 className='editTitle'>Edit user info</h1>
+
+                        <div className='editProfil-container'>
+                            <label className="editProfillabel">UserName : </label>
+                            <input
+                                className="editinput"
+                                type="text"
+                                placeholder="User Name"
+                                defaultValue={userName}
+                            />
+                        </div>
+
+                        <div className='editProfil-container'>
+                            <label className="editProfillabel">First Name: </label>
+                            <input
+                                className="editinput"
+                                type="text"
+                                placeholder="First Name"
+                                defaultValue={firstName}
+                            />
+                        </div>
+
+                        <div className='editProfil-container'>
+                            <label className="editProfillabel">Last Name: </label>
+                            <input
+                                className="editinput"
+                                type="text"
+                                placeholder="Last Name"
+                                defaultValue={lastName}
+                            />
+                        </div>
+                        <div className="editProfilButtonContainer">
+                            <button className="editProfilButton" type="submit">
+                                Save
+                            </button>
+                            <button className="editProfilButton" onClick={onCancel}>
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
                 </form>
-            </div>}
+            )}
         </div>
     );
 }
